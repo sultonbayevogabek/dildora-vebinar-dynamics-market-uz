36edac3b23ec840b33964b65212ebeefd62aec64
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-04-03 21:09:00 +0500

    commit

--- js/register.js
+++ js/register.js
@@ -9,7 +9,7 @@
     joinButton.disabled = true;
     await statistics.onClickTgBtn();
     joinButton.disabled = false;
-    window.location.href = 'https://t.me/dildoratursunovaofficial';
+    window.location.href = 'https://t.me/dildoratursunovaseminar';
   })
 
   if (user && user?.name && user?.phone && user?.time) {
